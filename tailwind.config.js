641ca1e6d0255eabfd92aecfa974ab037bf344d5
Rhara Ianna <rharaianna@gmail.com>
2023-04-26 14:57:32 -0300

    lista de post não responsiva

---
+++ tailwind.config.js
@@ -0,0 +1,19 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ['./app/**/*.html'],
+  theme: {
+    colors:{
+      'roxo-claro-1':'#E0AAFF',
+      'roxo-claro-2':'#C77DFF',
+      'roxo-medio-1':'#9D4EDD',
+      'roxo-medio-2': '#7B2CBF',
+      'roxo-escuro-1':'#5A189A',
+      'roxo-escuro-2':'#3C096C',
+      'azul-escuro-1':'#240046',
+      'azul-escuro-2':'#10002B',
+    },
+    extend: {},
+  },
+  plugins: [],
+}
+
