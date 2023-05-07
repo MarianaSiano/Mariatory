3c5cf4d9d37296cbe9ed936301af8f0f17216851
Ricardo <ricardoervilha456@gmail.com>
2023-05-07 00:57:04 -0300

    Adição dos modais de cadastro, edição e visualização de posts.

--- public/js/formPosts.js
+++ public/js/formPosts.js
@@ -1,21 +1,51 @@
 //Funcionamento do modal
-const fade = document.querySelector('.fade');
+const fadeExcluir = document.querySelector('.excluir');
+const fadeCadastrar = document.querySelector('.cadastrar');
+const fadeEditar = document.querySelector('.editar');
+const fadeVisualizar = document.querySelector('.visualizar');
 
-function openModal(){
-    fade.classList.add('actived');
+function openModalExcluir(){
+    fadeExcluir.classList.add('actived');
 }
 
-function closeModal(){
-    fade.classList.remove('actived');
+function closeModalExcluir(){
+    fadeExcluir.classList.remove('actived');
+}
+
+function openModalCadastrar(){
+    fadeCadastrar.classList.add('actived');
+}
+
+function closeModalCadastrar(){
+    fadeCadastrar.classList.remove('actived');
+}
+
+function openModalEditar(){
+    console.log('Foda-se')
+    fadeEditar.classList.add('actived');
+}
+
+function closeModalCadastrar(){
+    fadeEditar.classList.remove('actived');
+}
+
+function openModalVisualizar(){
+    console.log('Foda-se')
+    fadeVisualizar.classList.add('actived');
+}
+
+function closeModalVisualizar(){
+    fadeVisualizar.classList.remove('actived');
 }
 
 window.addEventListener('click', (event) => {
-    if(event.target == fade){
-        fade.classList.remove('actived');
+    if(event.target == fadeExcluir){
+        fadeExcluir.classList.remove('actived');
+    }else if(event.target == fadeCadastrar){
+        fadeCadastrar.classList.remove('actived');
+    }else if(event.target == fadeEditar){
+        fadeEditar.classList.remove('actived');
+    }else if(event.target == fadeVisualizar){
+        fadeVisualizar.classList.remove('actived');
     }
 })
-
-function pseudoDelete(){
-    fade.classList.remove('actived');
-    window.alert('Post deletado com sucesso!');
-}
