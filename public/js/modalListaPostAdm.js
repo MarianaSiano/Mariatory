507ec82c543da7018fc2c2a3946401a5a20ce286
Rhara Ianna <rharaianna@gmail.com>
2023-05-08 15:39:34 -0300

    modal criação de postagem

--- public/js/modalListaPostAdm.js
+++ public/js/modalListaPostAdm.js
@@ -58,3 +58,141 @@ modalVisualizacao.onclick = function (event) {
     modalVisualizacao.close();
   }
 };
+//-------------------------------------------------------------
+
+//Modal de Criação---------------------------------------------
+var modalVisualizacao = document.getElementById("modalCriacao");
+var botaoVisualizar = document.getElementById("botaoCriar");
+
+botaoCriar.onclick = function () {
+  modalCriacao.showModal();
+};
+
+modalCriacao.onclick = function (event) {
+  if (event.target == modalCriacao) {
+    modalCriacao.close();
+  }
+};
+
+//ammknjwabgui//
+
+const ul = document.querySelector("ul"),
+  input = document.querySelector("input"),
+  tagNumb = document.querySelector(".details span");
+
+let maxTags = 5,
+  tags = [];
+
+countTags();
+createTag();
+
+function countTags() {
+  input.focus();
+  tagNumb.innerText = maxTags - tags.length;
+}
+
+function createTag() {
+  ul.querySelectorAll("li").forEach((li) => li.remove());
+  tags
+    .slice()
+    .reverse()
+    .forEach((tag) => {
+      let liTag = `<li class="bg-roxo-claro-1 text-white rounded-lg px-2">${tag} <i class="uit uit-multiply" onclick="remove(this, '${tag}')"></i></li>`;
+      ul.insertAdjacentHTML("afterbegin", liTag);
+    });
+  countTags();
+}
+
+function remove(element, tag) {
+  let index = tags.indexOf(tag);
+  tags = [...tags.slice(0, index), ...tags.slice(index + 1)];
+  element.parentElement.remove();
+  countTags();
+}
+
+function addTag(e) {
+  if (e.key == "Enter") {
+    let tag = e.target.value.replace(/\s+/g, " ");
+    if (tag.length > 1 && !tags.includes(tag)) {
+      if (tags.length < 5) {
+        tag.split(",").forEach((tag) => {
+          tags.push(tag);
+          createTag();
+        });
+      }
+    }
+    e.target.value = "";
+  }
+}
+
+input.addEventListener("keyup", addTag);
+
+const removeBtn = document.querySelector(".details button");
+removeBtn.addEventListener("click", () => {
+  tags.length = 0;
+  ul.querySelectorAll("li").forEach((li) => li.remove());
+  countTags();
+});
+
+//-------------------------------------------------------------
+
+//Modal de Gênero---------------------------------------------
+/*
+const ul = document.querySelector("ul"),
+  input = document.querySelector("input"),
+  tagNumb = document.querySelector(".details span");
+
+let maxTags = 5,
+  tags = [];
+
+countTags();
+createTag();
+
+function countTags() {
+  input.focus();
+  tagNumb.innerText = maxTags - tags.length;
+}
+
+function createTag() {
+  ul.querySelectorAll("li").forEach((li) => li.remove());
+  tags
+    .slice()
+    .reverse()
+    .forEach((tag) => {
+      let liTag = `<li>${tag} <span class="x" onclick="remove(this, '${tag}')"></span></li>`;
+      ul.insertAdjacentHTML("afterbegin", liTag);
+    });
+  countTags();
+}
+
+function remove(element, tag) {
+  let index = tags.indexOf(tag);
+  tags = [...tags.slice(0, index), ...tags.slice(index + 1)];
+  element.parentElement.remove();
+  countTags();
+}
+
+function addTag(e) {
+  if (e.key == "Enter") {
+    let tag = e.target.value.replace(/\s+/g, " ");
+    if (tag.length > 1 && !tags.includes(tag)) {
+      if (tags.length < 5) {
+        tag.split(",").forEach((tag) => {
+          tags.push(tag);
+          createTag();
+        });
+      }
+    }
+    e.target.value = "";
+  }
+}
+
+input.addEventListener("keyup", addTag);
+
+const removeBtn = document.querySelector(".details button");
+removeBtn.addEventListener("click", () => {
+  tags.length = 0;
+  ul.querySelectorAll("li").forEach((li) => li.remove());
+  countTags();
+});
+*/
