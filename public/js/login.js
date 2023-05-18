70f913cec137744815e349439945699b82a7c805
Leandro Alvares <leleoac@gmail.com>
2023-05-18 17:56:14 -0300

    subindo pra main

---
+++ public/js/login.js
@@ -0,0 +1,7 @@
+document.getElementById('olho').addEventListener('mousedown', function() {
+    document.getElementById('senha').type = 'text';
+  });
+  
+  document.getElementById('olho').addEventListener('mouseup', function() {
+    document.getElementById('senha').type = 'password';
+  });
