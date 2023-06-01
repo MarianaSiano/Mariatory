97d72f950ffff402e81f437d6278e973e587ddce
ViitNunes <vitorianunes313@gmail.com>
2023-06-01 11:32:12 -0300

    alteraçao do html para php

---
+++ app/Controllers/ExampleController.php
@@ -0,0 +1,15 @@
+<?php
+
+namespace App\Controllers;
+
+use App\Core\App;
+use Exception;
+
+class ExampleController
+{
+    public function teste() 
+    { 
+        return view('site/login');
+    }
+
+}
