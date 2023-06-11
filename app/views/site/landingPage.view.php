19d7a9f0ed214101df943a30118c32737f78833f
Rhara Ianna <rharaianna@gmail.com>
2023-06-10 21:18:17 -0300

    aa

--- app/views/site/landingPage.view.php
+++ app/views/site/landingPage.view.php
@@ -193,43 +193,45 @@
             </li>
             <li>
                 <div class="post">
-                    <a href="#">
-                        <img src="../../../public/assets/livro1.png" alt="Livro O pequeno principe" width="210" ;>
-                    </a>
+                    <div class="centralizar">
+                        <div class="todosAlinhamentos">
+                            <div class="imagem">
+                                <img src="../../../public/assets/osmeh.jpg" alt="capa do livro">
+                            </div>
+                            <div class="alinhamento">
+                                <h3 class="titulosesquerda"> Título </h3>
+                                <p> Os 7 maridos de Evelyn Hugo </p>
+                            </div>
+                            <div class="alinhamento">
+                                <h3 class="titulosesquerda"> Autor </h3>
+                                <p> Taylor Jenkins Reid </p>
+                            </div>
+                            <div class="alinhamento">
+                                <h3 class="titulosesquerda"> Gênero </h3>
+                                <div class="classificacao">
+                                    <p class="elementos">Romance</p>
+                                    <p class="elementos">Psicológico</p>
+                                    <p class="elementos">Ficção</p>
+                                </div>
+                            </div>
+                            <div class="alinhamento">
+                                <h3 class="titulosesquerda"> Redflags </h3>
+                                <div class="classificacao">
+                                    <p class="elementos">cc</p>
+                                    <p class="elementos">bb</p>
+                                    <p class="elementos">aa</p>
+                                </div>
+                            </div>
+                            <div class="alinhamento">
+                                <h3 class="titulosesquerda"> Avaliação </h3>
+                                <p id="estrelas">&starf;&starf;&starf;&star;&star;</p>
+                            </div>
+                        </div>
+                    </div>
                 </div>
             </li>
 
-            <li>
-                <div class="post">
-                    <a href="#">
-                        <img src="../../../public/assets/livro2.png" alt="Livro Harry Potter e a pedra filosofal" width="210">
-                    </a>
-                </div>
-            </li>
 
-            <li>
-                <div class="post">
-                    <a href="#">
-                        <img src="../../../public/assets/livro3.png" alt="Livro Outros jeitos de usar a boca" width="210">
-                    </a>
-                </div>
-            </li>
-
-            <li>
-                <div class="post">
-                    <a href="#">
-                        <img src="../../../public/assets/livro4.png" alt="Livro Percy jackson e os olimpianos" width="210">
-                    </a>
-                </div>
-            </li>
-
-            <li>
-                <div class="post">
-                    <a href="#">
-                        <img src="../../../public/assets/livro5.png" alt="Livro frenesi historias de duplo terror" width="210">
-                    </a>
-                </div>
-            </li>
         </ul>
 
 
