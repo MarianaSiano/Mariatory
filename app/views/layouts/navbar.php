12dc36b0536309c850e2c67bcfee92d9afb8429a
vitor-frnds <vitor.fernandes@estudante.ufjf.br>
2023-06-12 16:41:57 -0300

    Versão final da navbar. Correção da sidebar para dispositivos móveis.

--- app/views/layouts/navbar.php
+++ app/views/layouts/navbar.php
@@ -8,34 +8,39 @@
     <link rel="stylesheet" href="/public/css/navbar.css">
     <link rel="icon" type="image/png" href="/public/assets/Maristory-Navbar.png" sizes="32x32">
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.2.1/css/all.min.css" integrity="sha512-MV7K8+y+gLIBoVD59lQIYicR65iaqukzvf/nwasF0nqhPay5w/9lJmVM2hMDcnK1OnMGCdVK+iQrJ7lzPJQd1w==" crossorigin="anonymous" referrerpolicy="no-referrer" />
+    <link href="https://cdn0.iconfinder.com/data/icons/ui-icons-pack/100/ui-icon-pack-14-512.png" rel='stylesheet'>
 
-    <title>Maristory</title>
 </head>
 
 <body>
-
     <header>
-        <nav>
-            <a class="logo" href="#"><img src="/public/assets/Maristory-Navbar-Transparente.png" alt="Maristory logo"></a>
-
-            <div class="mobile-menu">
-                <div class="line1"></div>
-                <div class="line2"></div>
-                <div class="line3"></div>
+        <nav class="nav-bar">
+            <div class="logo">
+                <img src="/public/assets/Maristory-Navbar-Transparente.png" alt="Maristory logo">
+            </div>
+            <div class="nav-list">
+                <ul>
+                    <li class="nav-item"><a href="#" class="nav-link">Página Inicial</a></li>
+                    <li class="nav-item"><a href="./app/views/site/postList.view.php" class="nav-link">Posts</a></li>
+                    <li class="nav-item"><a href="./app/views/site/login.view.php" class="nav-link">Login</a></li>
+                </ul>
             </div>
 
-            <ul class="nav-list">
-                <li><a href="#">Página Inicial</a></li>
-                <li><a href="./app/views/site/postList.view.php">Posts</a></li>
-                <li><a href="./app/views/site/login.view.php">Login / Cadastro</a></li>
-            </ul>
+            <div class="mobile-menu-icon">
+                <button onclick="menuShow()"><img class="icon" id="icon" src="/public/assets/menu_purple_36dp.svg" alt=""></button>
+            </div>
         </nav>
+        <div class="mobile-menu">
+            <ul>
+                <li class="nav-item"><a href="#" class="nav-link">Página Inicial</a></li>
+                <li class="nav-item"><a href="./app/views/site/postList.view.php" class="nav-link">Posts</a></li>
+                <li class="nav-item"><a href="./app/views/site/login.view.php" class="nav-link">Login</a></li>
+            </ul>
+
+        </div>
     </header>
 
-    <!--FIM HEADER-->
-
     <script src="./public/js/navbar.js"></script>
-
 </body>
 
 </html>
