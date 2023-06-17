93438913214285fd784860b3437f3e5180fcd73b
Rhara Ianna <rharaianna@gmail.com>
2023-06-17 19:59:21 -0300

    Botao nos comentários

--- app/views/site/vpost.view.php
+++ app/views/site/vpost.view.php
@@ -1,61 +1,69 @@
 <!DOCTYPE html>
 <html lang="en">
+
 <head>
     <title>Resenha - Nome Livro</title>
 
     <link rel="stylesheet" href="../../../public/css/vpost.css">
-   
+
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
 
 </head>
+
 <body>
-    <?php 
-        if(isset($post) || empty($post)) {
-            $post = new stdclass();
-        }
+    <?php
+    if (isset($post) || empty($post)) {
+        $post = new stdclass();
+    }
     ?>
-    <div class = "tudo">
+    <div class="tudo">
         <div class="conteudo">
-            <div class = "esquerda">
+            <div class="esquerda">
                 <div class="centralizar">
                     <img src=<?= '../../../' . ($post->image ?? 'public/images/posts/comprovante.png') ?> alt="capa do livro">
                     <div class="todosAlinhamentos">
-                        <div class = "alinhamento" > 
-                            <h3 class = "titulosesquerda"> Autor </h3>
+                        <div class="alinhamento">
+                            <h3 class="titulosesquerda"> Autor </h3>
                             <p class="textinhoesquerda"> <?= $post->author->name ?? '' ?> </p>
                         </div>
                         <div class="alinhamento">
-                            <h3 class = "titulosesquerda"> Gênero </h3>
+                            <h3 class="titulosesquerda"> Gênero </h3>
                             <div class="classificacao">
                                 <p class="elementos">Romance</p>
-                                
+
                             </div>
                         </div>
-                        
+
+
                         <div class="alinhamento">
-                            <h3 class = "titulosesquerda"> Avaliação </h3>
-                            <p class="textinhoesquerda" id="estrelas"><?= $post->rating ?? '' ?></p>
+                            <h3 class="titulosesquerda"> Avaliação </h3>
+                            <p class="textinhoesquerda" id="estrelas">&starf;&starf;&starf;&star;&star;</p>
                         </div>
                     </div>
                 </div>
             </div>
-            <div class = "direita">
+            <div class="direita">
                 <div class="textodireita">
-                    <h1 class="titulo"><?= $post->title ?? '' ?></h1> 
-                    <hr/>
+                    <h1 class="titulo"><?= $post->title ?? '' ?></h1>
+                    <hr />
                     <h2 class="h2">Sinopse</h2>
-                    <p class="texto"><?= $post->synopsis ?? '' ?></p>    
+                    <p class="texto"><?= $post->synopsis ?? '' ?></p>
                     <h2 class="h2">Resenha</h2>
-                    <p class="texto"><?= $post->review ?? '' ?></p>  
+                    <p class="texto"><?= $post->review ?? '' ?></p>
                 </div>
-                <div class = "comentarios">
+                <div class="comentarios">
                     <h2 id="titulocomentarios"> Comentários </h2>
-                    <div class = "espacocomentarios">
-                        <input type="text" class="text" placeholder="Comente aqui"/>
+                    <div class="espacocomentarios">
+                        <input type="text" class="text" placeholder="Nome de usuário" />
                     </div>
-                    <div class = "outroscomentarios">
+                    <button> Salvar </button>
+                    <div class="espacocomentarios">
+                        <input type="text" class="text" placeholder="Comente aqui" />
+                    </div>
+                    <button> Enviar </button>
+                    <div class="outroscomentarios">
                         <p><strong>@nomeusuário</strong><br>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. </p>
                         <p><strong>@nomeusuário</strong><br>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. </p>
                         <p><strong>@nomeusuário</strong><br>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. </p>
@@ -67,4 +75,5 @@
         </div>
     </div>
 </body>
+
 </html>
